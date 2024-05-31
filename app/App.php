49c65aeccb0e977515e44b55f437220845800350
hestlage <hestlage@student.21-school.ru>
2024-05-31 13:46:58 +0300

    'get_csv_files()' function has been added.

--- app/App.php
+++ app/App.php
@@ -2,4 +2,29 @@
 
 declare(strict_types = 1);
 
-// Your Code
+/**
+ * Extracts full paths of .csv files from the given directory.
+ * 
+ * @param string $dir Given directory.
+ * @return array $files Full paths of .csv files.
+ */
+function get_csv_files(string $dir) {
+    $files = [];
+    $content = scandir($dir);
+
+    foreach($content as $name) {
+
+        if ($name === '.' || $name === '..') continue;
+
+        $full_path = $dir . $name;
+        $extension = substr($name, -4);
+
+        if (is_file($full_path) && $extension === ".csv") {
+            $files[] = $full_path;
+        }
+
+    }
+
+    return $files;
+}
+?>
