e343d466f87136bfb8c24cfb674378010d5a8ed7
hestlage <hestlage@student.21-school.ru>
2024-05-31 14:55:30 +0300

    Entrypoint

--- public/index.php
+++ public/index.php
@@ -8,4 +8,16 @@ define('APP_PATH', $root . 'app' . DIRECTORY_SEPARATOR);
 define('FILES_PATH', $root . 'transaction_files' . DIRECTORY_SEPARATOR);
 define('VIEWS_PATH', $root . 'views' . DIRECTORY_SEPARATOR);
 
-/* YOUR CODE (Instructions in README.md) */
+require_once(APP_PATH . "App.php");
+
+$csv_files = get_csv_files(FILES_PATH);
+$data = extract_data($csv_files);
+[$total_income_val, $total_expense_val] = calculate_total($data);
+$net_total_val = $total_income_val - $total_expense_val;
+
+// formatting
+$total_income = "$" .  $total_income_val;
+$total_expense = "-$" .  $total_expense_val;
+$net_total = "$" .  $net_total_val;
+
+require_once(VIEWS_PATH . "transactions.php");
