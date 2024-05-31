1db16893df49e60c7afa05143dfc6578696d1d25
hestlage <hestlage@student.21-school.ru>
2024-05-31 13:47:48 +0300

    'extract_data()' function has been added.

--- app/App.php
+++ app/App.php
@@ -27,4 +27,35 @@ function get_csv_files(string $dir) {
 
     return $files;
 }
+
+/**
+ * Extracts data from .csv files into associative array.
+ * 
+ * @param array $csv_files
+ * @param array $data
+ */
+function extract_data($csv_files) {
+    $data = [];
+
+    foreach ($csv_files as $filename) {
+
+        if (($file = fopen($filename, "r")) !== false) {
+            $headers = fgetcsv($file);
+    
+            while (($line = fgetcsv($file)) !== false) {
+                // Date formatting
+                $date = date_create($line[0]);
+                $line[0] = date_format($date, "M j, Y");
+    
+                $data[] = array_combine($headers, $line);
+    
+            }
+    
+            fclose($file);
+        }
+
+    }
+
+    return $data;
+}
 ?>
